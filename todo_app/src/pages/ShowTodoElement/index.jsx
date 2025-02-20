c0eabb27d87d2bd7ef43167d30be03401222711f
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-02-21 00:53:48 +0700

    draw ui for todo item

--- todo_app/src/pages/ShowTodoElement/index.jsx
+++ todo_app/src/pages/ShowTodoElement/index.jsx
@@ -1,7 +1,35 @@
+import { useParams } from "react-router-dom";
+import { getTodoElement } from "../../helpers/GetTodoElement"; 
+import "./ShowTodoElement.scss";
 function ShowTodoElement(){
+    const params = useParams();
+    // console.log(params.id);
+    const list = getTodoElement(params.id);
+    console.log(list);
     return (
         <>
-            Todo Element
+            <div className="ShowTodoItem">
+                <div className="todoItem">
+                    <h3 className="todoItem__title">
+                        <div className="inner-title">Chủ đề :</div>
+                        <div className="inner-response">{list.title}</div> 
+                    </h3>
+                    <div className="todoItem__date">
+                        <div className="inner-title">Ngày tạo :</div>
+                        <div className="inner-response">{list.dateCreated}</div> 
+                    </div>
+                    <ul className="todoItem__list">
+                        <h3>Việc cần làm: </h3>
+                        {list.notes.map((item, index) => (
+                            <li key={index} className="todoItem__list--item">
+                                <button className="checkBox"></button>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            
         </>
     );
 }
